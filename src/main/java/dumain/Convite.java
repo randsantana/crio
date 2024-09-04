a630b1b6adf2b0ca159e26f9bb4c4f8c6ce19b9d
randsantana <randerson_santana@estudante.sesisenai.org.br>
2024-09-04 19:36:47 -0300

    feat: add class Invate

---
+++ src/main/java/dumain/Convite.java
@@ -0,0 +1,24 @@
+package dumain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "convite")
+@Entity
+public class Convite {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private UUID id;
+
+    private boolean confirmacao_presenca;
+}
